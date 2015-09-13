a2887f3a0dfe8075726d226517375c75bcb0be7b
Zhenx Tang <moumoon711@gmail.com>
2015-09-14 00:00:12 +0800

    c

---
+++ blog/models/db.js
@@ -0,0 +1,6 @@
+/**
+ * Created by ShrimpTang on 2015/9/13.
+ */
+var setting = require('../setting'),
+    Db = require('mongodb').Db,
+    Connection = require('mongodb').MongoClient
---
+++ blog/setting.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ShrimpTang on 2015/9/13.
+ */
+module.exports = {
+    cookieSecret: 'myblog',
+    db: 'blog',
+    host: 'localhost',
+    port: 27017
+};
